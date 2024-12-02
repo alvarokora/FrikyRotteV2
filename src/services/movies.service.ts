33224251c8299481333208990ce983506b372c1c
Alvaro <alvaromora50@gmail.com>
2024-12-02 17:36:18 -0300

    servicio movil

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -6,12 +6,29 @@ import { MovieModel } from "src/app/model/MovieModel";
 @Injectable({providedIn: 'root'})
 
 export class MoviesService{
+    
+    /*
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTkzYzQwYTA1NTBmNGFmNzIwMTFlYTI0ZjhiNzc3NyIsIm5iZiI6MTczMjE5NzQ0Ny4xOTYwOTI0LCJzdWIiOiI2NzNlODg2NjIxZGE0Mjk2N2ZjNDgyMzkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.rRc6EeTGcpsJcee4wGilduVnXBtQmu_44AsQWp62uO8'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+        */
+
     apiUrl = "https://api.themoviedb.org/3";
-    apiKey = "7193c40a0550f4af72011ea24f8b7777";
+    apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTkzYzQwYTA1NTBmNGFmNzIwMTFlYTI0ZjhiNzc3NyIsIm5iZiI6MTczMjE5NzQ0Ny4xOTYwOTI0LCJzdWIiOiI2NzNlODg2NjIxZGE0Mjk2N2ZjNDgyMzkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.rRc6EeTGcpsJcee4wGilduVnXBtQmu_44AsQWp62uO8";
 
     constructor(private http: HttpClient){}
 
     getPopularMovies(): Observable<MovieModel[]>{
-        return this.http.get<MovieModel[]>(`${this.apiUrl}/movie/popular?api_key=${this.apiKey}`);
+        return this.http.get<MovieModel[]>(`${this.apiUrl}/trending/movie/day?language=en-USapi_key=${this.apiKey}`);
     }
+
 }
