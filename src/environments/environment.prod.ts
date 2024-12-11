f00b032fcf84aceafdef465546f96da938d183c9
Alvaro <alvaromora50@gmail.com>
2024-12-10 22:18:07 -0300

    Android Studio Funcional 😎

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPEsc6ozT78v5iHGVcU92uewzrBf85WhU",
+    authDomain: "frikyrottev2.firebaseapp.com",
+    projectId: "frikyrottev2",
+    storageBucket: "frikyrottev2.firebasestorage.app",
+    messagingSenderId: "653590668033",
+    appId: "1:653590668033:web:04601d040ef191a4e6b100",
+    measurementId: "G-6Y74XB6HY4"
+  },
+  tmdb: {
+    apiKey: '7193c40a0550f4af72011ea24f8b7777',
+    baseUrl: 'https://api.themoviedb.org/3'
+  },
+  rawg: {
+    apiKey: 'afe4a0d2f86a492d9598ebe0590fa6ba',
+    baseUrl: 'https://api.rawg.io/api',
+  }
+
 };
